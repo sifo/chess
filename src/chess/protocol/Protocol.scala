5fe24687794aef6c1c48e8f5dee1473df17468cd
loic <loic@1b087311-5d0e-48b6-b328-a1455b69f0bc>
2011-12-01 15:08:41 +0000

    Ajout de quelques lignes pour ne plus avoir d'erreurs, les lignes ajoutées ne sont absolument pas suffisantes pour que ça fonctionne.
    git-svn-id: http://anand.dev.regis-gianas.com/svn@17 1b087311-5d0e-48b6-b328-a1455b69f0bc

--- src/chess/protocol/Protocol.scala
+++ src/chess/protocol/Protocol.scala
@@ -1,7 +1,12 @@
+package chess.protocol;
+import chess.event.Event
+
 object Protocol {
 	def translateIn (s : String) : Event = {
+	  new Event;
 	}
 	
 	def translateOut (e : Event) : String = {
+	  new String;
 	}
 }
