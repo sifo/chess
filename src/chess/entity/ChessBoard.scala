e3535f6f3c93c6180659791911320652c0302b66
loic <loic@1b087311-5d0e-48b6-b328-a1455b69f0bc>
2011-12-04 11:36:16 +0000

    git-svn-id: http://anand.dev.regis-gianas.com/svn@31 1b087311-5d0e-48b6-b328-a1455b69f0bc

--- src/chess/entity/ChessBoard.scala
+++ src/chess/entity/ChessBoard.scala
@@ -6,35 +6,35 @@ package chess.entity;
 //			Squares are an array in the Board
 
 abstract class Board {
-	/**
-	 * Returns true if the move was successful, false otherwise
-	 */
-	def move() : Boolean;
-	
-	/**
-	 * Returns the winner, 0 if no one has won yet.
-	 */
-	def doWeHaveAWinner() : Int;
+  /**
+   * Returns true if the move was successful, false otherwise
+   */
+  def move(xSrc: Int, ySrc: Int, xDst: Int, yDst: Int): Boolean;
+
+  /**
+   * Returns the winner, 0 if no one has won yet.
+   */
+  def doWeHaveAWinner(): Int;
 }
 
 object Board {
-  
+
   def apply() = {
     new ChessBoard(8);
   }
 }
 
-class ChessBoard(dimension : Int) extends Board {
+class ChessBoard(dimension: Int) extends Board {
   var squares = Array.ofDim[Int](dimension, dimension);
   var pieces = Array.ofDim[Piece](dimension, dimension);
-  var winner : Int = 0;
-  
-  override def move() = {
+  var winner: Int = 0;
+
+  override def move(xSrc: Int, ySrc: Int, xDst: Int, yDst: Int) = {
     true;
   }
-  
+
   override def doWeHaveAWinner() = {
     winner;
   }
-  
+
 }
