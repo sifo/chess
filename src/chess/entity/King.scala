eaa2a3eea2a7ce3f774a576a034624fdfa013d6a
ibrahim <ibrahim@1b087311-5d0e-48b6-b328-a1455b69f0bc>
2011-12-04 21:42:55 +0000

    git-svn-id: http://anand.dev.regis-gianas.com/svn@36 1b087311-5d0e-48b6-b328-a1455b69f0bc

--- src/chess/entity/King.scala
+++ src/chess/entity/King.scala
@@ -1,5 +1,8 @@
 package chess.entity
+import chess.behavior.move.CastlingMove
+import chess.behavior.move.AnySimpleMove
+import chess.behavior.promote.NoWayPromote
 
-class King extends Piece {
+class King extends Piece with NoWayPromote with CastlingMove with AnySimpleMove{
 
 }
