d644d57195efd9b66f5a4cf890a32cc26d8cec0c
loic <loic@1b087311-5d0e-48b6-b328-a1455b69f0bc>
2011-12-05 14:13:14 +0000

    Ajout des méthodes add et remove à Board, plus implémentations dans ChessBoard
    git-svn-id: http://anand.dev.regis-gianas.com/svn@41 1b087311-5d0e-48b6-b328-a1455b69f0bc

--- src/chess/entity/ChessBoard.scala
+++ src/chess/entity/ChessBoard.scala
@@ -7,9 +7,21 @@ package chess.entity;
 
 abstract class Board {
   /**
+   * Adds a Piece to a position
+   * Returns true if the Piece could be placed, false otherwise
+   */
+  def add(p: Piece, pos: Position): Boolean;
+
+  /**
+   * Removes a Piece from a position
+   * Returns success
+   */
+  def remove(p : Position) : Boolean;
+  
+  /**
    * Returns true if the move was successful, false otherwise
    */
-  def move(xSrc: Int, ySrc: Int, xDst: Int, yDst: Int): Boolean;
+  def move(src: Position, dst: Position): Boolean;
 
   /**
    * Returns the winner, 0 if no one has won yet.
@@ -28,13 +40,26 @@ class ChessBoard(dimension: Int) extends Board {
   var squares = Array.ofDim[Piece](dimension, dimension);
   var winner: Int = 0;
 
-  override def move(xSrc: Int, ySrc: Int, xDst: Int, yDst: Int) = {
-	  if(! squares(xSrc)(ySrc).isInstanceOf[Piece] ) {
-		  false;
-	  }
-	  else {
-	  	squares(xSrc)(ySrc).canMove(new Position(xDst, yDst));
-	  }
+  override def add(p: Piece, pos: Position): Boolean = {
+    if (squares(pos.x)(pos.y) != null) {
+      squares(pos.x).update(pos.y, p);
+      true;
+    } else {
+      false;
+    }
+  }
+
+  override def remove(pos : Position) : Boolean = {
+    squares(pos.x).update(pos.y, null);
+    true;
+  }
+  
+  override def move(src: Position, dst: Position) = {
+    if (!squares(src.x)(src.y).isInstanceOf[Piece]) {
+      false;
+    } else {
+      squares(src.x)(src.y).canMove(dst);
+    }
   }
 
   override def doWeHaveAWinner() = {
