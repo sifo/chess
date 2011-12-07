6ca71870e9cdb097271db130676b9491b3e6e9ee
ibrahim <ibrahim@1b087311-5d0e-48b6-b328-a1455b69f0bc>
2011-12-07 09:06:53 +0000

    git-svn-id: http://anand.dev.regis-gianas.com/svn@45 1b087311-5d0e-48b6-b328-a1455b69f0bc

--- src/chess/entity/King.scala
+++ src/chess/entity/King.scala
@@ -6,4 +6,24 @@ import chess.behavior.promote.NoWayPromote
 class King extends Piece {
 	var moveBehavior = new AnySimpleMove with CastlingMove {} 
 	var promoteBehavior = new NoWayPromote {}
+	var position
+	var boardDimension
+	var color
+	
+	
+	override def canMove(init: Position): Boolean = {
+		moveBehavior.canMove(init, getPosition, boardDimension)
+	}
+
+	override def canPromote = promoteBehavior.canPromote
+	override def getMoveBehavior = moveBehavior
+	override def getPromoteBehavior = promoteBehavior
+	override def getPosition = position
+	override def getColor = color
+	
+	override def setMoveBehavior(mb: MoveBehavior) = moveBehavior = mb
+	override def setPromoteBehavior(pb: PromoteBehavior) = promoteBehavior = pb
+	override def setPosition(pos: Position) = position = pos
+	override def setBoardDimension(dim : Int) = boardDimension = dim
+	override def setColor(c : Int) = color = c
 }
