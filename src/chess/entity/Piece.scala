b3dd65d2a99a3d085432f3f83ea987b81d130115
ibrahim <ibrahim@1b087311-5d0e-48b6-b328-a1455b69f0bc>
2011-12-07 09:05:55 +0000

    git-svn-id: http://anand.dev.regis-gianas.com/svn@43 1b087311-5d0e-48b6-b328-a1455b69f0bc

--- src/chess/entity/Piece.scala
+++ src/chess/entity/Piece.scala
@@ -3,9 +3,10 @@ import chess.behavior.promote.PromoteBehavior
 import chess.behavior.move.MoveBehavior
 
 abstract class Piece {
-	private var moveBehavior: MoveBehavior = null
+	private var moveBehavior   : MoveBehavior    = null
 	private var promoteBehavior: PromoteBehavior = null
 	private var position: Position = new Position(-1, -1)
+	private var color : Int = -1	// la couleur et un INT, où le 0 == BLANC et le 1 == NOIR
 	
 	//Ajout pour pouvoir transmettre la dimension
 	//aux canMove(). Nécessaire, sinon, risques de sortir du plateau.
@@ -17,16 +18,14 @@ abstract class Piece {
 	}
 
 	def canPromote = promoteBehavior.canPromote
-
 	def getMoveBehavior = moveBehavior
-
 	def getPromoteBehavior = promoteBehavior
-	
 	def getPosition = position
+	def getColor = color
 
 	def setMoveBehavior(mb: MoveBehavior) = moveBehavior = mb
-
 	def setPromoteBehavior(pb: PromoteBehavior) = promoteBehavior = pb
-	
 	def setPosition(pos: Position) = position = pos
+	def setBoardDimension(dim : Int) = boardDimension = dim
+	def setColor(c : Int) = color = c
 }
