686db606f906bb4a906bc9d4dbfc248e573d1925
loic <loic@1b087311-5d0e-48b6-b328-a1455b69f0bc>
2011-12-08 12:32:08 +0000

    Ajout de la méthode getPiece() dans ChessBoard et Board, pour avoir accès au contenu du plateau (le tableau contenant le plateau est dans l'implémentation de Board, et non dans Board, en raison de la possibilité de ne pas avoir un plateau à 2 dimensions). Du coup, doWeHaveAWinner n'est probablement pas utile..
    git-svn-id: http://anand.dev.regis-gianas.com/svn@51 1b087311-5d0e-48b6-b328-a1455b69f0bc

--- src/chess/entity/ChessBoard.scala
+++ src/chess/entity/ChessBoard.scala
@@ -16,16 +16,25 @@ abstract class Board {
    * Removes a Piece from a position
    * Returns success
    */
-  def remove(p : Position) : Boolean;
-  
+  def remove(p: Position): Boolean;
+
   /**
    * Returns true if the move was successful, false otherwise
    */
   def move(src: Position, dst: Position): Boolean;
 
   /**
+   * Returns the Piece at given Position
+   */
+  def getPiece(pos: Position): Piece;
+
+  /**
    * Returns the winner, 0 if no one has won yet.
    */
+  //Utile ? Si l'état de la partie est déterminé dans BoardManager,
+  //cette méthode est inutile, puisque le manager peut accéder aux
+  //pieces via la méthode getPiece(), et demander leurs mouvements
+  //possibles.
   def doWeHaveAWinner(): Int;
 }
 
@@ -49,11 +58,11 @@ class ChessBoard(dimension: Int) extends Board {
     }
   }
 
-  override def remove(pos : Position) : Boolean = {
+  override def remove(pos: Position): Boolean = {
     squares(pos.x).update(pos.y, null);
     true;
   }
-  
+
   override def move(src: Position, dst: Position) = {
     if (!squares(src.x)(src.y).isInstanceOf[Piece]) {
       false;
@@ -62,6 +71,10 @@ class ChessBoard(dimension: Int) extends Board {
     }
   }
 
+  override def getPiece(pos: Position) : Piece = {
+    squares(pos.x)(pos.y)
+  }
+  
   override def doWeHaveAWinner() = {
     winner;
   }
