cc717732ed7211d303cfa1dd22ef532da5fd07e5
loic <loic@1b087311-5d0e-48b6-b328-a1455b69f0bc>
2011-12-08 13:41:20 +0000

    Commentaires
    git-svn-id: http://anand.dev.regis-gianas.com/svn@52 1b087311-5d0e-48b6-b328-a1455b69f0bc

--- src/chess/behavior/move/MoveBehavior.scala
+++ src/chess/behavior/move/MoveBehavior.scala
@@ -2,14 +2,26 @@ package chess.behavior.move;
 import chess.entity.Position
 
 trait MoveBehavior {
+  /**
+   * Returns whether the piece can move to the given position from its
+   * initial position.
+   */
   //Dimension permet de passer la dimension du plateau, inconnue autrement
   //puisqu'on utilise un trait.
   def canMove(init: Position, dest: Position, dimension : Int): Boolean = {
     false
   }
 
+  /**
+   * Returns an array containing 0s everywhere, and 1s where the piece can
+   * move, starting from the given position, on a board of given dimension
+   */
   def possibleMoves(init: Position, dimension: Int): Array[Array[Int]];
 
+  /**
+   * Returns an array of threatening moves. For most of the pieces in a chess
+   * game, it is the same as possibleMoves(), except for the Pawn.
+   */
   def threateningMoves(init: Position, dimension: Int): Array[Array[Int]] =
     possibleMoves(init, dimension);
 }
