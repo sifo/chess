6abde761eff765ecab8f362d17d6cac0dc5b6823
ibrahim <ibrahim@1b087311-5d0e-48b6-b328-a1455b69f0bc>
2011-12-09 14:21:32 +0000

    remplacer les paramètres des fonctions par un seul (Action)
    git-svn-id: http://anand.dev.regis-gianas.com/svn@54 1b087311-5d0e-48b6-b328-a1455b69f0bc

--- src/chess/behavior/move/MoveBehavior.scala
+++ src/chess/behavior/move/MoveBehavior.scala
@@ -1,5 +1,6 @@
 package chess.behavior.move;
 import chess.entity.Position
+import chess.history.Action
 
 trait MoveBehavior {
   /**
@@ -8,7 +9,7 @@ trait MoveBehavior {
    */
   //Dimension permet de passer la dimension du plateau, inconnue autrement
   //puisqu'on utilise un trait.
-  def canMove(init: Position, dest: Position, dimension : Int): Boolean = {
+  def canMove(action: Action): Boolean = {
     false
   }
 
@@ -16,12 +17,12 @@ trait MoveBehavior {
    * Returns an array containing 0s everywhere, and 1s where the piece can
    * move, starting from the given position, on a board of given dimension
    */
-  def possibleMoves(init: Position, dimension: Int): Array[Array[Int]];
+  def possibleMoves(action: Action): Array[Array[Int]];
 
   /**
    * Returns an array of threatening moves. For most of the pieces in a chess
    * game, it is the same as possibleMoves(), except for the Pawn.
    */
-  def threateningMoves(init: Position, dimension: Int): Array[Array[Int]] =
-    possibleMoves(init, dimension);
+  def threateningMoves(action: Action): Array[Array[Int]] =
+    possibleMoves(action);
 }
