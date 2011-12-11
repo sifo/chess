4af8a6cc89790d96a66c06c86b2c23a3f83bb3c7
ibrahim <ibrahim@1b087311-5d0e-48b6-b328-a1455b69f0bc>
2011-12-11 15:36:57 +0000

    git-svn-id: http://anand.dev.regis-gianas.com/svn@56 1b087311-5d0e-48b6-b328-a1455b69f0bc

---
+++ src/chess/history/Action.scala
@@ -0,0 +1,9 @@
+package chess.history
+import java.awt.Dimension
+import chess.entity.Position
+import chess.entity.Piece
+
+
+class Action(var src: Position, var dst: Position, var piece: Piece, var dim: Dimension) {
+
+}
