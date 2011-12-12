efd21a57e07a069311eeacac1d41ea193b91bc46
ibrahim <ibrahim@1b087311-5d0e-48b6-b328-a1455b69f0bc>
2011-12-12 09:14:19 +0000

    true if instanceOf == Pawn
    git-svn-id: http://anand.dev.regis-gianas.com/svn@59 1b087311-5d0e-48b6-b328-a1455b69f0bc

--- src/chess/behavior/promote/StrandardPromote.scala
+++ src/chess/behavior/promote/StrandardPromote.scala
@@ -1,7 +1,11 @@
 package chess.behavior.promote
 
+import chess.history.Action
+import chess.entity.Pawn 
+
 trait StandardPromote extends PromoteBehavior {
-	override def canPromote(): Boolean = {
-		false
+  
+	override def canPromote(action: Action): Boolean = {
+	  if(action.piece.isInstanceOf[Pawn]) true else false
 	}
 }
