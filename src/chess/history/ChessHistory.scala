115f432867ed7b420fa884515f488b77721aa34d
ibrahim <ibrahim@1b087311-5d0e-48b6-b328-a1455b69f0bc>
2011-12-12 10:32:16 +0000

    la classe (totalité) ChessHistory
    git-svn-id: http://anand.dev.regis-gianas.com/svn@69 1b087311-5d0e-48b6-b328-a1455b69f0bc

--- src/chess/history/ChessHistory.scala
+++ src/chess/history/ChessHistory.scala
@@ -1,5 +1,29 @@
 package chess.history
 
 class ChessHistory extends History {
-
+  var listeActions = List[Action]()
+  
+  def addAction(action: Action) {
+    listeActions = action :: listeActions
+  }
+  
+  def getAction(indice: Int) : Action = {
+    if ((indice + 1) > listeActions.length) getLastInternal(listeActions)
+    else getActionInternal(listeActions, indice)
+  }
+  
+  def getLastAction() : Action = {
+      getLastInternal(listeActions)
+  }
+  
+  def getLastInternal(liste: List[Action]) : Action = liste match {
+    case (a :: Nil) => a
+    case (a1 :: a2 :: xs) => getLastInternal(a2 :: xs)
+    case _ => null
+  }
+  
+  def getActionInternal(liste: List[Action], i: Int) : Action = (liste, i) match {
+    case (a :: xs, 0) => a
+    case (a :: xs, i) => getActionInternal(xs, i-1)
+  }
 }
