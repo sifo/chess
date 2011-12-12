216b4471cad63b337e231e019ec2d75ea9656207
ibrahim <ibrahim@1b087311-5d0e-48b6-b328-a1455b69f0bc>
2011-12-12 09:18:12 +0000

    supprimer les anciens ajouts
    git-svn-id: http://anand.dev.regis-gianas.com/svn@62 1b087311-5d0e-48b6-b328-a1455b69f0bc

--- src/chess/entity/Pawn.scala
+++ src/chess/entity/Pawn.scala
@@ -1,28 +1,10 @@
 package chess.entity
+
 import chess.behavior.move.SimpleMove
 import chess.behavior.promote.StandardPromote
 
-class Pawn extends Piece {
-	val moveBehavior = new SimpleMove {}
-	var promoteBehavior = new StandardPromote {}
-	var position
-	var boardDimension
-	var color
-	
-	
-	override def canMove(init: Position): Boolean = {
-		moveBehavior.canMove(init, getPosition, boardDimension)
-	}
 
-	override def canPromote = promoteBehavior.canPromote
-	override def getMoveBehavior = moveBehavior
-	override def getPromoteBehavior = promoteBehavior
-	override def getPosition = position
-	override def getColor = color
-	
-	override def setMoveBehavior(mb: MoveBehavior) = moveBehavior = mb
-	override def setPromoteBehavior(pb: PromoteBehavior) = promoteBehavior = pb
-	override def setPosition(pos: Position) = position = pos
-	override def setBoardDimension(dim : Int) = boardDimension = dim
-	override def setColor(c : Int) = color = c
+class Pawn extends Piece {
+	var moveBehavior = new SimpleMove {}
+	var promoteBehavior = new StandardPromote {}
 }
