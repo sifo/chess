5de0555853fdb2c5effc434044ba7007c85225cf
ibrahim <ibrahim@1b087311-5d0e-48b6-b328-a1455b69f0bc>
2011-12-12 09:58:02 +0000

    trait hestory
    git-svn-id: http://anand.dev.regis-gianas.com/svn@66 1b087311-5d0e-48b6-b328-a1455b69f0bc

--- src/chess/history/History.scala
+++ src/chess/history/History.scala
@@ -1,5 +1,10 @@
-package chess.history;
+package chess.history
+
+import chess.history.Action  	// J'arrive pas à résoudre ce problème l'hors de l'importation
+								// Meme problème pour la class Color dans Piece
 
 trait History {
-	
+	def addAction(action: Action)
+	def getAction(indice: Int) : Action
+	def getLastAction() : Action
 }
