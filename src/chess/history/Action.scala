53e6b0f9a9a27373819a0fc63b3e360b213c3a54
jean <jean@1b087311-5d0e-48b6-b328-a1455b69f0bc>
2011-12-19 13:42:46 +0000

    Enlève la dimension dans Action
    git-svn-id: http://anand.dev.regis-gianas.com/svn@77 1b087311-5d0e-48b6-b328-a1455b69f0bc

--- src/chess/history/Action.scala
+++ src/chess/history/Action.scala
@@ -4,6 +4,6 @@ import chess.entity.Position
 import chess.entity.Piece
 
 
-class Action(var src: Position, var dst: Position, var piece: Piece, var dim: Dimension) {
+class Action(var src: Position, var dst: Position, var piece: Piece) {
 
 }
