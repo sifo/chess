91a81432d21a46734d29841fefdcea54698516b3
jean <jean@1b087311-5d0e-48b6-b328-a1455b69f0bc>
2011-12-25 19:32:44 +0000

    Complète VerticalMove
    git-svn-id: http://anand.dev.regis-gianas.com/svn@92 1b087311-5d0e-48b6-b328-a1455b69f0bc

--- src/chess/behavior/move/VerticalMove.scala
+++ src/chess/behavior/move/VerticalMove.scala
@@ -4,7 +4,16 @@ import chess.entity.MovementInfo
 
 trait VerticalMove extends MoveBehavior {
 	
-	override def canMove(movementInfo : MovementInfo): Boolean = {
+	override def canMove(movementInfo: MovementInfo): Boolean = {
+		val dest = movementInfo.dst
+		val pos = movementInfo.src
+		val dim = movementInfo.boardDim
+		if (!dim.isInBounds(dest) || pos.equals(dest)) {
+			return super.canMove(movementInfo)
+		}
+		if (dest.x == pos.x && dest.y != pos.y) {
+			return true
+		}
 		super.canMove(movementInfo)
 	}
 }
