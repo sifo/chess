8cde58dd476d30719c8cd2417b090623a43814d4
jean <jean@1b087311-5d0e-48b6-b328-a1455b69f0bc>
2011-12-25 19:32:36 +0000

    Complète HorizontalMove
    git-svn-id: http://anand.dev.regis-gianas.com/svn@90 1b087311-5d0e-48b6-b328-a1455b69f0bc

--- src/chess/behavior/move/HorizontalMove.scala
+++ src/chess/behavior/move/HorizontalMove.scala
@@ -4,7 +4,16 @@ import chess.entity.MovementInfo
 
 trait HorizontalMove extends MoveBehavior {
 	
-	override def canMove(movementInfo : MovementInfo): Boolean = {
+	override def canMove(movementInfo: MovementInfo): Boolean = {
+		val dest = movementInfo.dst
+		val pos = movementInfo.src
+		val dim = movementInfo.boardDim
+		if (!dim.isInBounds(dest) || pos.equals(dest)) {
+			return super.canMove(movementInfo)
+		}
+		if (dest.y == pos.y && dest.x != pos.x) {
+			return true
+		}
 		super.canMove(movementInfo)
 	}
 }
