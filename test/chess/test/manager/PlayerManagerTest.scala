a74af70391f129382724bb5c36bfcae6e6d04ee6
jean <jean@1b087311-5d0e-48b6-b328-a1455b69f0bc>
2012-01-01 17:10:29 +0000

    Ajoute des tests pour le player manager
    git-svn-id: http://anand.dev.regis-gianas.com/svn@95 1b087311-5d0e-48b6-b328-a1455b69f0bc

--- test/chess/test/manager/PlayerManagerTest.scala
+++ test/chess/test/manager/PlayerManagerTest.scala
@@ -1,5 +1,44 @@
 package chess.test.manager
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.matchers.ShouldMatchers
+import org.junit.runner.RunWith
+import org.scalatest.Spec
+import org.scalatest.BeforeAndAfter
+import chess.entity.Player
+import chess.entity.Color._
+import chess.manager.PlayerManager
+import chess.ChessModel
 
-class PlayeManagerTest {
+@RunWith(classOf[JUnitRunner])
+class PlayerManagerTest extends Spec with BeforeAndAfter {
 	
+	var playerManager: PlayerManager = _
+	var chessModel: ChessModel = _
+	
+	before {
+		chessModel = new ChessModel()
+		playerManager = new PlayerManager(chessModel)
+	}
+	
+	describe("PlayerManager") {
+		
+		it("should accept more players in game") {
+			var player = new Player("name", Black)
+			assert(!playerManager.players.contains(player))
+			playerManager.add(player)
+			assert(playerManager.players.contains(player))
+		}
+		
+		it("should increment currentPlayerIndex after setting next player") {
+			playerManager.currentPlayerIndex = 0
+			playerManager.setNextPlayer()
+			assert(playerManager.currentPlayerIndex == 1)
+		}
+		
+		it("should put currentPlayerIndex at 0 if the index was on last element") {
+			playerManager.currentPlayerIndex = playerManager.players.size - 1
+			playerManager.setNextPlayer()
+			assert(playerManager.currentPlayerIndex == 0)
+		}
+	}
 }
