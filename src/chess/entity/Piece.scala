de123935bfebdd58b6f28ec9869a96e2aa6f2bb7
jean <jean@1b087311-5d0e-48b6-b328-a1455b69f0bc>
2012-01-04 19:46:22 +0000

    Enlève getters et setters dans la classe Piece
    En scala, les getters et setters ne sont pas pertinents, contrairement à
    ceux en java.
    
    git-svn-id: http://anand.dev.regis-gianas.com/svn@105 1b087311-5d0e-48b6-b328-a1455b69f0bc

--- src/chess/entity/Piece.scala
+++ src/chess/entity/Piece.scala
@@ -10,7 +10,7 @@ abstract class Piece {
 	var moveBehavior   : MoveBehavior    = null
 	var promoteBehavior: PromoteBehavior = null
 	var position: Position = new Position(-1, -1)
-	var boardDimension : Dimension = null
+	var board : Dimension = null
 	var color : Color = null
 	var mvtInfo: MovementInfo = null
 	
@@ -24,25 +24,5 @@ abstract class Piece {
 	
 	def canPromote(movementInfo: MovementInfo): Boolean = {
 	  promoteBehavior.canPromote(movementInfo)
-	}
-	
-	def getMoveBehavior = moveBehavior
-	def getPromoteBehavior = promoteBehavior
-	def getBoardDimension = boardDimension
-	def getPosition = position
-	def getColor = color
-	def getAction = mvtInfo
-	
-
-	def setMoveBehavior(mb: MoveBehavior) = moveBehavior = mb
-	def setPromoteBehavior(pb: PromoteBehavior) = promoteBehavior = pb
-	def setPosition(pos: Position) = position = pos
-	def setBoardDimension(dim : Dimension) = boardDimension = dim
-	def setColor(c : Color) = color = c
-	def setMvtInfo(dest: Position) {
-	  mvtInfo.src   = getPosition
-	  mvtInfo.dst   = dest
-	  mvtInfo.piece = this
-	  mvtInfo.boardDim = getBoardDimension
 	}
 }
