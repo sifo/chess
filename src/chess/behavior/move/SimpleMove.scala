f8f39c3d20f2dd0b589ec53cd8c6c665a2bb54a2
ibrahim <ibrahim@1b087311-5d0e-48b6-b328-a1455b69f0bc>
2012-01-05 17:49:44 +0000

    classe SimpleMove
    git-svn-id: http://anand.dev.regis-gianas.com/svn@116 1b087311-5d0e-48b6-b328-a1455b69f0bc

--- src/chess/behavior/move/SimpleMove.scala
+++ src/chess/behavior/move/SimpleMove.scala
@@ -1,10 +1,45 @@
-package chess.behavior.move;
+package chess.behavior.move
 import chess.entity.Position
 import chess.entity.MovementInfo
+import chess.entity.Color
+import chess.entity.Pawn
 
 trait SimpleMove extends MoveBehavior {
+  
+	override def possibleMoves(mvtInfo: MovementInfo): Array[Array[Int]] = {
+	  var output = Array.fill[Int](mvtInfo.chessBoard.dimension.height, mvtInfo.chessBoard.dimension.width)(0);
+	  val action = mvtInfo.history.getLastAction()
+	  
+	  if ((mvtInfo.src.y + 1 < mvtInfo.chessBoard.dimension.width) && (mvtInfo.piece.color == White))
+	    output(mvtInfo.src.x)(mvtInfo.src.y + 1) = 1
+	    
+	  if ((mvtInfo.src.y - 1 >= 0) && (mvtInfo.piece.color == Black))
+	    output(mvtInfo.src.x)(mvtInfo.src.y - 1) = 1
+	  
+	  if ((mvtInfo.src.y == 1) && (mvtInfo.piece.color == White))
+	    output(mvtInfo.src.x)(3) = 1
+	  
+	  if ((mvtInfo.src.y == 6) && (mvtInfo.piece.color == Black))
+	    output(mvtInfo.src.x)(4) = 1
+	  
+	  if ((action.piece.isInstanceOf[Pawn]) && (mvtInfo.piece.color == White)) {
+	    if ((action.src.y == 6) && (action.dst.y == 4)) {
+	      if ((mvtInfo.src.y == 4) && ((mvtInfo.src.x + 1 == action.src.x) || (mvtInfo.src.x - 1 == action.src.x)))
+	          output(action.src.x)(5) = 1
+	    }
+	  }
+	  
+	  if ((action.piece.isInstanceOf[Pawn]) && (mvtInfo.piece.color == White)) {
+	    if ((action.src.y == 6) && (action.dst.y == 4)) {
+	      if ((mvtInfo.src.y == 4) && ((mvtInfo.src.x + 1 == action.src.x) || (mvtInfo.src.x - 1 == action.src.x)))
+	          output(action.src.x)(5) = 1
+	    }
+	  }
+	  
+	  output;
+	}
 	
-	override def canMove(movementInfo : MovementInfo): Boolean = {
-		super.canMove(movementInfo)
+	override def canMove(mvtInfo: MovementInfo): Boolean = {
+	  possibleMoves(mvtInfo)(mvtInfo.dst.x)(mvtInfo.dst.y) == 1;
 	}
 }
