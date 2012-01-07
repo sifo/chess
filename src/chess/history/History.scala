2b4b57d27b49f62d477131259f3e17a4913d657c
ibrahim <ibrahim@1b087311-5d0e-48b6-b328-a1455b69f0bc>
2012-01-07 21:27:43 +0000

    ajout de la fonction "changeStat"
    git-svn-id: http://anand.dev.regis-gianas.com/svn@132 1b087311-5d0e-48b6-b328-a1455b69f0bc

--- src/chess/history/History.scala
+++ src/chess/history/History.scala
@@ -3,6 +3,7 @@ package chess.history
 
 trait History {
 	def addAction(action: Action)
+	def changeStat(action: Action)			// Changer l'état des pièces, utilisée dans CastlingMove
 	def getAction(indice: Int) : Action
 	def getLastAction() : Action
 }
