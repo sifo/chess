f4e596aeaebfef39f0c42bc222b42c36dd5f3c34
jean <jean@1b087311-5d0e-48b6-b328-a1455b69f0bc>
2012-01-07 15:15:50 +0000

    Permet de charger différentes configurations pour jouer
    git-svn-id: http://anand.dev.regis-gianas.com/svn@131 1b087311-5d0e-48b6-b328-a1455b69f0bc

--- src/chess/Main.scala
+++ src/chess/Main.scala
@@ -2,13 +2,10 @@ package chess
 
 object Main {
 	def main(args: Array[String]) {
-		var config = "res/standard-chess-config.xml"
-		if(!args.isEmpty) {
-			config = args(0)
-		}
-		val chessModel = new ChessModel(config);
-		val chessController = new ChessController(chessModel);
-		chessController.displayViews();
-		chessModel.start();
+		var config = if(!args.isEmpty) args(0) else "res/standard-chess-config.xml"
+		val chessModel = new ChessModel(config)
+		val chessController = new ChessController(chessModel)
+		chessController.displayViews()
+		chessModel.start()
 	}
 }
