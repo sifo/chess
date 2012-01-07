7d7ea08f339484cb26bbfdc7e4020c1064794a40
ibrahim <ibrahim@1b087311-5d0e-48b6-b328-a1455b69f0bc>
2012-01-07 21:36:19 +0000

    ajout de qlqs fcts et attribus étulisés dans le CastlingMove
    git-svn-id: http://anand.dev.regis-gianas.com/svn@133 1b087311-5d0e-48b6-b328-a1455b69f0bc

--- src/chess/history/ChessHistory.scala
+++ src/chess/history/ChessHistory.scala
@@ -1,10 +1,21 @@
 package chess.history
+import chess.entity.King
+import chess.entity.Rook
+import chess.entity.Color._
 
 class ChessHistory extends History {
   private var listeActions = List[Action]()
   
+  private var kingWhiteInit  = true
+  private var rookLWhiteInit = true
+  private var rookRWhiteInit = true
+  private var kingBlackInit  = true
+  private var rookLBlackInit = true
+  private var rookRBlackInit = true
+  
   def addAction(action: Action) {
     listeActions = action :: listeActions
+    changeStat(action)
   }
   
   def getAction(indice: Int) : Action = {
@@ -26,4 +37,41 @@ class ChessHistory extends History {
     case (a :: xs, 0) => a
     case (a :: xs, i) => getActionInternal(xs, i-1)
   }
+  
+  /*
+   * Fonctions utilisées dans le CastlingMove
+   * Enregistrer et récupérer l'état des pièces concernées par le CastlingMove
+   *  
+   */
+  
+  def changeStat(action: Action) {
+    if(action.piece.isInstanceOf[King] && action.piece.color == White)
+      kingWhiteInit = false
+    else if(action.piece.isInstanceOf[Rook] && action.piece.color == White
+        && action.src.x == 0
+        && action.src.y == 0)
+      rookLWhiteInit = false
+    else if(action.piece.isInstanceOf[Rook] && action.piece.color == White
+        && action.src.x == 7
+        && action.src.y == 0)
+      rookRWhiteInit = false
+    
+    else if(action.piece.isInstanceOf[King] && action.piece.color == Black)
+      kingBlackInit = false
+    else if(action.piece.isInstanceOf[Rook] && action.piece.color == Black
+        && action.src.x == 0
+        && action.src.y == 7)
+      rookLBlackInit = false
+    else if(action.piece.isInstanceOf[Rook] && action.piece.color == Black
+        && action.src.x == 7
+        && action.src.y == 7)
+      rookRBlackInit = false
+  }
+  
+  def getKingWhiteStat  = kingWhiteInit
+  def getrookLWhiteStat = rookLWhiteInit
+  def getrookRWhiteStat = rookRWhiteInit
+  def getKingBlackStat  = kingBlackInit
+  def getrookLBlackStat = rookLBlackInit
+  def getrookRBlackStat = rookRBlackInit 
 }
