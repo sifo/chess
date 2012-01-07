631c09b35a70e046e6d44a7ee46388fbe776ee51
ibrahim <ibrahim@1b087311-5d0e-48b6-b328-a1455b69f0bc>
2012-01-07 21:47:02 +0000

    CastlingMove
    git-svn-id: http://anand.dev.regis-gianas.com/svn@134 1b087311-5d0e-48b6-b328-a1455b69f0bc

--- src/chess/behavior/move/CastlingMove.scala
+++ src/chess/behavior/move/CastlingMove.scala
@@ -1,10 +1,36 @@
 package chess.behavior.move;
 import chess.entity.Position
 import chess.entity.MovementInfo
+import chess.entity.Color._
+import chess.history.ChessHistory
 
 trait CastlingMove extends MoveBehavior {
-	
-	override def canMove(movementInfo : MovementInfo): Boolean = {
-		super.canMove(movementInfo)
+		
+	override def canMove(mvtInfo: MovementInfo): Boolean = {
+	  
+	  if((mvtInfo.piece.color == White) && mvtInfo.history.getKingWhiteStat) {
+	    if ((mvtInfo.src.x == 4) && (mvtInfo.src.y == 0)) {
+	      // Petit Roque
+	      if((mvtInfo.dst.x == 6) && (mvtInfo.dst.y == 0) && mvtInfo.history.getrookRWhiteStat)
+	        true
+	      // Grand Roque
+	      else if ((mvtInfo.dst.x == 2) && (mvtInfo.dst.y == 0) && mvtInfo.history.getrookLWhiteStat) {
+	        true
+	      }
+	    }
+	  }
+	  
+	  else if((mvtInfo.piece.color == Black) && mvtInfo.history.getKingBlackStat) {
+	    if ((mvtInfo.src.x == 4) && (mvtInfo.src.y == 7)) {
+	      // Petit Roque
+	      if((mvtInfo.dst.x == 6) && (mvtInfo.dst.y == 7) && mvtInfo.history.getrookRBlackStat)
+	        true
+	      // Grand Roque
+	      else if ((mvtInfo.dst.x == 2) && (mvtInfo.dst.y == 7) && mvtInfo.history.getrookLBlackStat)
+	        true
+	    }
+	  }
+	  
+	  false
 	}
 }
