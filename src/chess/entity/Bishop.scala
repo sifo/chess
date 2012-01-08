bf00ac21352617888567b0edc559b2b664eedf36
jean <jean@1b087311-5d0e-48b6-b328-a1455b69f0bc>
2012-01-08 23:39:58 +0000

    Corrige le comportement de promotion du Fou
    git-svn-id: http://anand.dev.regis-gianas.com/svn@153 1b087311-5d0e-48b6-b328-a1455b69f0bc

--- src/chess/entity/Bishop.scala
+++ src/chess/entity/Bishop.scala
@@ -1,10 +1,12 @@
 package chess.entity
 import chess.behavior.move.DiagonalMove
 import chess.behavior.move.MoveBehavior
+import chess.behavior.promote.PromoteBehavior
+import chess.behavior.promote.NoWayPromote
 
 class Bishop extends Piece {
-	moveBehavior = new MoveBehavior with DiagonalMove
-	promoteBehavior = null
 	
+	moveBehavior = new MoveBehavior with DiagonalMove
+	promoteBehavior = new PromoteBehavior with NoWayPromote
 	override def name() = "Bishop";
 }
