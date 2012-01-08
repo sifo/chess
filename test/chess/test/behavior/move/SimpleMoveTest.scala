2b8832e95616bad0d43b5813f7a408ac06afa6cd
ibrahim <ibrahim@1b087311-5d0e-48b6-b328-a1455b69f0bc>
2012-01-08 15:56:21 +0000

    git-svn-id: http://anand.dev.regis-gianas.com/svn@147 1b087311-5d0e-48b6-b328-a1455b69f0bc

--- test/chess/test/behavior/move/SimpleMoveTest.scala
+++ test/chess/test/behavior/move/SimpleMoveTest.scala
@@ -9,6 +9,10 @@ import chess.entity.ChessBoard
 import chess.entity.Dimension
 import chess.entity.MovementInfo
 import chess.entity.Position
+import chess.entity.Pawn
+import chess.history.ChessHistory
+import chess.history.Action
+
 
 @RunWith(classOf[JUnitRunner])
 class SimpleMoveTest extends Spec with BeforeAndAfter {
@@ -18,25 +22,54 @@ class SimpleMoveTest extends Spec with BeforeAndAfter {
 	var src: Position = _
 	var dst: Position = _
 	var mvtInfo: MovementInfo = _
+	var chessHistory : ChessHistory = _
+	var action : Action = _
+	var pawn : Pawn = _
 
 	before {
-		movement = new MoveBehavior with SimpleMove
+		movement   = new MoveBehavior with SimpleMove
 		chessBoard = new ChessBoard(new Dimension(8, 8))
-		src = new Position(4, 0)
+		src   = new Position(4, 1)
+		src_2 = new Position(4, 2)
+		src_3 = new Position(4, 4)
 	}
 	
 	describe("SimpleMove") {
 		
-		it("should refuse ") {
-			assert(true)
+		it("should reject move on the same position") {
+			dst = new Position(4, 1)
+			mvtInfo = new MovementInfo(src, dst, null, chessBoard, null)
+			assert(!movement.canMove(mvtInfo))
 		}
 		
-		it("should accept 1") {
-			assert(true)
+		it("should reject move more than one square (outside the second column)") {
+			dst = new Position(4, 4)
+			mvtInfo = new MovementInfo(src_2, dst, null, chessBoard, null)
+			assert(!movement.canMove(mvtInfo))
 		}
 		
-		it("should accept 2") {
-			assert(true)
+		
+		it("should accept simple move") {
+			dst = new Position(4, 2)
+			mvtInfo = new MovementInfo(src, dst, null, chessBoard, null)
+			assert(movement.canMove(mvtInfo))
+		}
+		
+		it("should accept move more than one square (inside the second column)") {
+			dst = new Position(4, 3)
+			mvtInfo = new MovementInfo(src, dst, null, chessBoard, null)
+			assert(movement.canMove(mvtInfo))
+		}
+		
+		it("should accept") {
+			chessHistory = new ChessHistory
+			pawn = new Pawn
+			action = new Action(new Position(5, 6), new Position(5, 4), pawn)
+			chessHistory.addAction(action)
+			dst = new Position(5, 5)
+			
+			mvtInfo = new MovementInfo(src, dst, null, chessBoard, chessHistory)
+			assert(movement.canMove(mvtInfo))
 		}
 	}
 }
