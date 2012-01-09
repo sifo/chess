2165562fbbb7cdbcbc9df181a1dca9ce838700d2
ibrahim <ibrahim@1b087311-5d0e-48b6-b328-a1455b69f0bc>
2012-01-09 21:48:30 +0000

    ajout de la fonction "getDirection" plus qlqs modifs
    git-svn-id: http://anand.dev.regis-gianas.com/svn@181 1b087311-5d0e-48b6-b328-a1455b69f0bc

--- src/chess/behavior/move/SimpleMove.scala
+++ src/chess/behavior/move/SimpleMove.scala
@@ -3,37 +3,65 @@ import chess.entity.Position
 import chess.entity.MovementInfo
 import chess.entity.Color._
 import chess.entity.Pawn
+import chess.history.Action
+import scala.xml.XML
 
 trait SimpleMove extends MoveBehavior {
+
+	private def getDirection(configFile: String, posit_x: Int, posit_y: Int): String = {
+		val config = XML.loadFile(configFile)
+		for (val entry <- config \\ "board-pieces" \ "piece-setup") {
+			val x = (entry \ "@x").text.toInt
+			val y = (entry \ "@y").text.toInt
+			if(x == posit_x && y == posit_y) {
+				val playerNumber = (entry \ "@player").text.toInt
+				for (val i <- config \\ "player-config" \ "player-direction") {
+					if((i \ "@player").text.toInt == playerNumber)
+					  return (i \ "@direction").text
+				}
+			}
+		}
+		"north"
+	}
   
 	override def possibleMoves(mvtInfo: MovementInfo): Array[Array[Int]] = {
 	  var output = Array.fill[Int](mvtInfo.chessBoard.dimension.height, mvtInfo.chessBoard.dimension.width)(0);
-	  val action = mvtInfo.history.getLastAction()
+	  val src = mvtInfo.src
+	  val direction = getDirection("res/standard-chess-config.xml", src.x, src.y)
+	  var lastMvt= new Action(null, null, null)
+	  lastMvt = mvtInfo.history.getLastAction()
+	  // val color  = mvtInfo.piece.color
 	  
-	  if ((mvtInfo.src.y + 1 < mvtInfo.chessBoard.dimension.width) && (mvtInfo.piece.color == White))
-	    output(mvtInfo.src.x)(mvtInfo.src.y + 1) = 1
+	  if ((src.y + 1 < mvtInfo.chessBoard.dimension.width) && (direction == "north")) //(color == White))
+	    output(src.x)(src.y + 1) = 1
 	    
-	  if ((mvtInfo.src.y - 1 >= 0) && (mvtInfo.piece.color == Black))
-	    output(mvtInfo.src.x)(mvtInfo.src.y - 1) = 1
+	  if ((src.y - 1 >= 0) && (direction == "south")) // (color == Black))
+	    output(src.x)(src.y - 1) = 1
 	  
-	  if ((mvtInfo.src.y == 1) && (mvtInfo.piece.color == White))
-	    output(mvtInfo.src.x)(3) = 1
+	  if ((src.y == 1) && (direction == "north"))
+	    output(src.x)(3) = 1
 	  
-	  if ((mvtInfo.src.y == 6) && (mvtInfo.piece.color == Black))
-	    output(mvtInfo.src.x)(4) = 1
+	  if ((src.y == 6) && (direction == "south"))
+	    output(src.x)(4) = 1
 	  
-	  if ((action.piece.isInstanceOf[Pawn]) && (mvtInfo.piece.color == White)) {
-	    if ((action.src.y == 6) && (action.dst.y == 4)) {
-	      if ((mvtInfo.src.y == 4) && ((mvtInfo.src.x + 1 == action.src.x) || (mvtInfo.src.x - 1 == action.src.x)))
-	          output(action.src.x)(5) = 1
-	    }
-	  }
+	  /*
+	   ************ PRISE EN PASSANT
+	   * */
 	  
-	  if ((action.piece.isInstanceOf[Pawn]) && (mvtInfo.piece.color == White)) {
-	    if ((action.src.y == 6) && (action.dst.y == 4)) {
-	      if ((mvtInfo.src.y == 4) && ((mvtInfo.src.x + 1 == action.src.x) || (mvtInfo.src.x - 1 == action.src.x)))
-	          output(action.src.x)(5) = 1
-	    }
+	  if(lastMvt != null) {
+		  if ((lastMvt.piece.isInstanceOf[Pawn]) && (direction == "north")) {
+		    if ((lastMvt.src.y == 6) && (lastMvt.dst.y == 4)) {
+		      if ((src.y == 4) && ((src.x + 1 == lastMvt.src.x) || (src.x - 1 == lastMvt.src.x)))
+		          output(lastMvt.src.x)(5) = 1
+		    }
+		  }
+		  
+		  if ((lastMvt.piece.isInstanceOf[Pawn]) && (direction == "south")) {
+		    if ((lastMvt.src.y == 1) && (lastMvt.dst.y == 3)) {
+		      if ((src.y == 3) && ((src.x + 1 == lastMvt.src.x) || (src.x - 1 == lastMvt.src.x)))
+		          output(lastMvt.src.x)(2) = 1
+		    }
+		  }
 	  }
 	  
 	  output;
