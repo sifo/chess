daef066cff646a0962f8ed54bbaee3c1e316554b
jean <jean@1b087311-5d0e-48b6-b328-a1455b69f0bc>
2012-01-09 19:50:05 +0000

    Ajoute des tests pour vérifier l'échec du roi
    git-svn-id: http://anand.dev.regis-gianas.com/svn@171 1b087311-5d0e-48b6-b328-a1455b69f0bc

--- test/chess/test/manager/CheckSituationTest.scala
+++ test/chess/test/manager/CheckSituationTest.scala
@@ -22,6 +22,7 @@ class CheckSituationTest extends Spec with BeforeAndAfter {
 	var boardManager: BoardManager = _
 	var chessModel: ChessModel = _
 	var rookWhite: Piece = _
+	var rookBlack: Piece = _
 	var kingWhite: Piece = _
 	var kingBlack: Piece = _
 
@@ -33,28 +34,32 @@ class CheckSituationTest extends Spec with BeforeAndAfter {
 		kingWhite.color = White
 		kingBlack = new King()
 		kingBlack.color = Black
+		rookBlack = new Rook()
+		rookBlack.color = Black
 		rookWhite.position = new Position(3, 1)
 		kingWhite.position = new Position(4, 0)
 		kingBlack.position = new Position(2, 5)
-		chessModel.boardManager.isVerticalConnected = true
+		rookBlack.position = new Position(5, 4)
 		boardManager = chessModel.boardManager
 		boardManager.board.squares(3)(1) = rookWhite
 		boardManager.board.squares(4)(0) = kingWhite
 		boardManager.board.squares(2)(5) = kingBlack
+		boardManager.board.squares(5)(4) = rookBlack
 		boardManager.pieces = rookWhite :: boardManager.pieces
 		boardManager.pieces = kingWhite :: boardManager.pieces
 		boardManager.pieces = kingBlack :: boardManager.pieces
+		boardManager.pieces = rookBlack :: boardManager.pieces
 	}
 
 	/*
-	 * k roi noir, R tour blanch, K roi noir.
+	 * k roi noir, R tour blanche, K roi blanc, r tour noire.
 	 * On bouge R en c2, et on regarde si le jeu repère si le roi
 	 * noir est en échec.
 	 * 
 	 * 8  . . . . . . . .
 	 * 7  . . . . . . . . 
 	 * 6  . . k . . . . . 
-	 * 5  . . . . . . . .
+	 * 5  . . . . . r . .
 	 * 4  . . . . . . . .
 	 * 3  . . . . . . . . 
 	 * 2  . . . R . . . .
@@ -62,13 +67,37 @@ class CheckSituationTest extends Spec with BeforeAndAfter {
 	 * 
 	 *    a b c d e f g h 
 	 */
-	
+
 	describe("CheckSituation") {
 
 		it("should change isCheck to true if there is a check situation") {
 			assert(boardManager.isCheck == false)
 			boardManager.move(new Position(2, 1), rookWhite)
 			assert(boardManager.isCheck == true)
+		}
+
+		it("should refuse move which not remove check situation") {
+			val dest = new Position(5, 6)
+			boardManager.move(new Position(2, 1), rookWhite)
+			boardManager.move(dest, rookBlack)
+			assert(boardManager.board.squares(dest.x)(dest.y) != rookBlack)
+			assert(chessModel.playerManager.currentPlayerIndex == 1)
+		}
+
+		it("should accept move which block check situation") {
+			val dest = new Position(2, 4)
+			boardManager.move(new Position(2, 1), rookWhite)
+			boardManager.move(dest, rookBlack)
+			assert(boardManager.board.squares(dest.x)(dest.y) == rookBlack)
+			assert(chessModel.playerManager.currentPlayerIndex == 0)
+		}
+
+		it("should accept move which remove check situation") {
+			val dest = new Position(3, 5)
+			boardManager.move(new Position(2, 1), rookWhite)
+			boardManager.move(dest, kingBlack)
+			assert(boardManager.board.squares(dest.x)(dest.y) == kingBlack)
+			assert(chessModel.playerManager.currentPlayerIndex == 0)
 		}
 	}
 }
