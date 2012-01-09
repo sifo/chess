70c1ec1e17f1400c5dcfb39e232a1341ad3257fb
loic <loic@1b087311-5d0e-48b6-b328-a1455b69f0bc>
2012-01-09 16:09:19 +0000

    DiagonalMove et LMove tests
    git-svn-id: http://anand.dev.regis-gianas.com/svn@159 1b087311-5d0e-48b6-b328-a1455b69f0bc

--- src/chess/behavior/move/DiagonalMove.scala
+++ src/chess/behavior/move/DiagonalMove.scala
@@ -6,53 +6,53 @@ import chess.entity.ChessBoard
 
 trait DiagonalMove extends MoveBehavior {
 
-	override def possibleMoves(mvtInfo: MovementInfo): Array[Array[Int]] = {
-		var output = Array.fill[Int](mvtInfo.chessBoard.dimension.width, mvtInfo.chessBoard.dimension.height)(0);
+  override def possibleMoves(mvtInfo: MovementInfo): Array[Array[Int]] = {
+    var output = Array.fill[Int](mvtInfo.chessBoard.dimension.width, mvtInfo.chessBoard.dimension.height)(0);
 
-		/*
+    /*
      * Fills the array with 1 where the DiagonalMove can be performed.
      * Is the piece would move out of the board, the exception is 
      * caught to do nothing
      */
-		if (mvtInfo.src.x + 1 < mvtInfo.chessBoard.dimension.width
-			&& mvtInfo.src.y + 1 < mvtInfo.chessBoard.dimension.height)
-			output(mvtInfo.src.x + 1)(mvtInfo.src.y + 1) = 1;
-		if (mvtInfo.src.x + 1 < mvtInfo.chessBoard.dimension.width
-			&& mvtInfo.src.y - 1 >= 0)
-			output(mvtInfo.src.x + 1)(mvtInfo.src.y - 1) = 1;
-		if (mvtInfo.src.x - 1 >= 0
-			&& mvtInfo.src.y + 1 < mvtInfo.chessBoard.dimension.height)
-			output(mvtInfo.src.x - 1)(mvtInfo.src.y + 1) = 1;
-		if (mvtInfo.src.x - 1 >= 0
-			&& mvtInfo.src.y - 1 >= 0)
-			output(mvtInfo.src.x - 1)(mvtInfo.src.y - 1) = 1;
+    if (mvtInfo.src.x + 1 < mvtInfo.chessBoard.dimension.width
+      && mvtInfo.src.y + 1 < mvtInfo.chessBoard.dimension.height)
+      output(mvtInfo.src.x + 1)(mvtInfo.src.y + 1) = 1;
+    if (mvtInfo.src.x + 1 < mvtInfo.chessBoard.dimension.width
+      && mvtInfo.src.y - 1 >= 0)
+      output(mvtInfo.src.x + 1)(mvtInfo.src.y - 1) = 1;
+    if (mvtInfo.src.x - 1 >= 0
+      && mvtInfo.src.y + 1 < mvtInfo.chessBoard.dimension.height)
+      output(mvtInfo.src.x - 1)(mvtInfo.src.y + 1) = 1;
+    if (mvtInfo.src.x - 1 >= 0
+      && mvtInfo.src.y - 1 >= 0)
+      output(mvtInfo.src.x - 1)(mvtInfo.src.y - 1) = 1;
 
-		output;
-	}
-	override def canMove(mvtInfo: MovementInfo): Boolean = {
-		val dest = mvtInfo.dst
-		val pos = mvtInfo.src
-		val board = mvtInfo.chessBoard
-		if (Math.abs(pos.x - dest.x) == Math.abs(pos.y - dest.y)) {
-			if (numberOfPieceOnPath(mvtInfo.chessBoard, pos, dest) == 0) {
-				return true
-			}
-		}
-		return super.canMove(mvtInfo)
-	}
+    output;
+  }
+  override def canMove(mvtInfo: MovementInfo): Boolean = {
+    val dest = mvtInfo.dst
+    val pos = mvtInfo.src
+    val board = mvtInfo.chessBoard
+    if (Math.abs(pos.x - dest.x) == Math.abs(pos.y - dest.y)) {
+      if (numberOfPieceOnPath(mvtInfo.chessBoard, pos, dest) == 0) {
+        return true
+      }
+    }
+    return super.canMove(mvtInfo)
+  }
 
-	private def numberOfPieceOnPath(
-		board: ChessBoard, pos: Position, dest: Position): Int = {
-		var min = Math.min(pos.x, dest.x) + 1
-		var max = Math.max(pos.x, dest.x) - 1
-		var res = 0
-		var y = pos.y
-		var decal = if (pos.y < dest.y) 1 else -1
-		for (i <- min to max) {
-			y = y + decal
-			if (board.squares(i)(y) != null)
-				res = res + 1
-		}
-		return res
-	}
+  private def numberOfPieceOnPath(
+    board: ChessBoard, pos: Position, dest: Position): Int = {
+    var min = Math.min(pos.x, dest.x) + 1
+    var max = Math.max(pos.x, dest.x) - 1
+    var res = 0
+    var y = pos.y
+    var decal = if (pos.y < dest.y) 1 else -1
+    for (i <- min to max) {
+      y = y + decal
+      if (board.squares(i)(y) != null)
+        res = res + 1
+    }
+    return res
+  }
 }
