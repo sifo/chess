331548cc242e48340b774bd2c4d43c0d93ee17a9
jean <jean@1b087311-5d0e-48b6-b328-a1455b69f0bc>
2012-01-14 18:48:23 +0000

    Implémente des requêtes venant des vues dans le ChessController
    git-svn-id: http://anand.dev.regis-gianas.com/svn@203 1b087311-5d0e-48b6-b328-a1455b69f0bc

--- src/chess/ChessController.scala
+++ src/chess/ChessController.scala
@@ -1,18 +1,33 @@
 package chess
 import chess.ui.TextView
 import chess.ui.events.DrawBoardEvent
+import chess.ui.events.MoveEvent
 
 class ChessController(var chessModel : ChessModel) {
 	var view = new TextView(this)
-	view.drawBoard(new DrawBoardEvent(chessModel.boardManager.board.dimension));
+	addListenersToModel()
 	
 	def addListenersToModel()= chessModel.addChessListener(view)
 	
-	def displayViews() = view.display
-
 	def closeViews() = view.close
+
+	def notifyMovement(moveEvent : MoveEvent) {
+		val src = moveEvent.src
+		if (!chessModel.boardManager.board.dimension.isInBounds(src)) {
+			chessModel.fireImpossibleMovement()
+			return
+		}
+		val piece = chessModel.boardManager.board.squares(src.x)(src.y)
+		if (piece == null) {
+			chessModel.fireNoPieceAtPosition()
+			return
+		}
+		chessModel.boardManager.move(moveEvent.dst, piece)
+	}
 	
-	def notifyQuitGame() = System.exit(0)
+	def notifyQuitGame() = {
+		chessModel.quit()
+	}
 	
 	def notifyNewGame() = chessModel.newGame
 }
