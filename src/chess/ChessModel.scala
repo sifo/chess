a5fa72b3e64263ae32b635961912998a91ab775f
jean <jean@1b087311-5d0e-48b6-b328-a1455b69f0bc>
2012-01-14 18:48:26 +0000

    Implémente davantage de méthodes pour communiquer avec les vues
    git-svn-id: http://anand.dev.regis-gianas.com/svn@204 1b087311-5d0e-48b6-b328-a1455b69f0bc

--- src/chess/ChessModel.scala
+++ src/chess/ChessModel.scala
@@ -2,6 +2,14 @@ package chess
 import chess.manager.BoardManager
 import chess.manager.PlayerManager
 import scala.collection.mutable.ListBuffer
+import chess.ui.events.DrawBoardEvent
+import chess.ui.events.CantPutYourselfInCheckEvent
+import chess.ui.events.CantMovePieceEvent
+import chess.ui.events.CheckEvent
+import chess.ui.events.CheckMateEvent
+import chess.ui.events.PatEvent
+import chess.ui.events.NotPlayerTurnEvent
+import chess.ui.events.NoPieceAtPositionEvent
 
 class ChessModel(var config: String) extends Game {
 
@@ -22,23 +30,52 @@ class ChessModel(var config: String) extends Game {
 	}
 	
 	def quit() {
-		
+		fireClose()
 	}
 	
 	def fireBoard() : Unit = {
-		
+		for(l <- listeners)
+			l.drawBoard(new DrawBoardEvent(boardManager.board))
+	}
+	
+	def fireClose() : Unit = {
+		for(l <- listeners)
+			l.close()
 	}
 	
 	def fireImpossibleMovement() : Unit = {
-		
+		for(l <- listeners)
+			l.cantMovePiece(new CantMovePieceEvent())
+	}
+	
+	def fireNotPlayerTurn() : Unit = {
+		for(l <- listeners)
+			l.notPlayerTurn(new NotPlayerTurnEvent())
+	}
+	
+	def firePlayerCantPutHimselfInCheck() : Unit = {
+		for(l <- listeners)
+			l.cantPutYourselfInCheck(new CantPutYourselfInCheckEvent())
+	}
+	
+	def fireCheck() : Unit = {
+		for(l <- listeners)
+			l.check(new CheckEvent())
 	}
 	
 	def fireCheckMate() : Unit = {
-		
+		for(l <- listeners)
+			l.checkmate(new CheckMateEvent())
 	}
 	
 	def firePat() : Unit = {
-		
+		for(l <- listeners)
+			l.pat(new PatEvent())
+	}
+	
+	def fireNoPieceAtPosition() : Unit = {
+		for(l <- listeners)
+			l.noPieceAtPosition(new NoPieceAtPositionEvent())
 	}
 	
 	def fireChangeCurrentPlayer() : Unit = {
@@ -46,17 +83,12 @@ class ChessModel(var config: String) extends Game {
 	}
 	
 	def start() {
-		//fireConfigDialog()
+		fireBoard()
 	}
 	
 	def newGame() {
-		start()
-	}
-	
-	def play() {
 		boardManager = new BoardManager(this)
 		playerManager = new PlayerManager(this)
-		fireBoard()
-		//boardManager.putPieceOnBoard
+		start()
 	}
 }
