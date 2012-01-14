2cc02b0adbf2cf9cd0d0cd5798c8aca89926ade0
jean <jean@1b087311-5d0e-48b6-b328-a1455b69f0bc>
2012-01-14 18:48:04 +0000

    Ajoute des méthodes pour les vues dans ChessListener
    git-svn-id: http://anand.dev.regis-gianas.com/svn@198 1b087311-5d0e-48b6-b328-a1455b69f0bc

--- src/chess/ChessListener.scala
+++ src/chess/ChessListener.scala
@@ -1,16 +1,32 @@
 package chess
 import chess.ui.events.MoveEvent
-import chess.ui.events.TellCantMovePieceEvent
 import chess.ui.events.PlacePieceOnBoardEvent
 import chess.ui.events.DrawBoardEvent
+import chess.ui.events.CantPutYourselfInCheckEvent
+import chess.ui.events.CantMovePieceEvent
+import chess.ui.events.CheckEvent
+import chess.ui.events.CheckMateEvent
+import chess.ui.events.PatEvent
+import chess.ui.events.NotPlayerTurnEvent
+import chess.ui.events.NoPieceAtPositionEvent
 
 trait ChessListener {
 	
-	def drawBoard(event : MoveEvent) : Unit
-	
 	def drawBoard(event : DrawBoardEvent) : Unit
 	
-	def tellCantMovePiece(event : TellCantMovePieceEvent) : Unit
+	def cantMovePiece(event : CantMovePieceEvent) : Unit
 	
-	def placePieceOnTile(event : PlacePieceOnBoardEvent) : Unit
+	def notPlayerTurn(event : NotPlayerTurnEvent) : Unit
+	
+	def noPieceAtPosition(event : NoPieceAtPositionEvent) : Unit
+	
+	def cantPutYourselfInCheck(event : CantPutYourselfInCheckEvent) : Unit
+	
+	def check(event : CheckEvent) : Unit
+	
+	def checkmate(event : CheckMateEvent) : Unit
+	
+	def pat(event : PatEvent) : Unit
+	
+	def close() : Unit
 }
